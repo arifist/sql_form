5045bb5099231fe38be7cc9ff5b71726906d1aa8
Arif Kalaycı <201118050@samsun.edu.tr>
2022-12-28 10:30:37 +0300

    sql_form

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtMaas = new System.Windows.Forms.TextBox();
+            this.dateDogumT = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCinsiyet = new System.Windows.Forms.TextBox();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNameFol = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(22, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Personel Ad";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(22, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Personel Soyad";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(22, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dogum Tarihi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(22, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Maas";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(205, 73);
+            this.txtAd.Multiline = true;
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(144, 25);
+            this.txtAd.TabIndex = 4;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(205, 120);
+            this.txtSoyad.Multiline = true;
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(144, 27);
+            this.txtSoyad.TabIndex = 5;
+            this.txtSoyad.TextChanged += new System.EventHandler(this.txtSoyad_TextChanged);
+            // 
+            // txtMaas
+            // 
+            this.txtMaas.Location = new System.Drawing.Point(205, 220);
+            this.txtMaas.Multiline = true;
+            this.txtMaas.Name = "txtMaas";
+            this.txtMaas.Size = new System.Drawing.Size(144, 25);
+            this.txtMaas.TabIndex = 6;
+            // 
+            // dateDogumT
+            // 
+            this.dateDogumT.Location = new System.Drawing.Point(205, 171);
+            this.dateDogumT.Name = "dateDogumT";
+            this.dateDogumT.Size = new System.Drawing.Size(144, 20);
+            this.dateDogumT.TabIndex = 7;
+            this.dateDogumT.ValueChanged += new System.EventHandler(this.dateDogumT_ValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 333);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1094, 328);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.Green;
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(418, 26);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(146, 53);
+            this.btnEkle.TabIndex = 9;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.Red;
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(418, 96);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(146, 55);
+            this.btnSil.TabIndex = 10;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGuncelle.Location = new System.Drawing.Point(418, 171);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(146, 56);
+            this.btnGuncelle.TabIndex = 11;
+            this.btnGuncelle.Text = "Guncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRead.Location = new System.Drawing.Point(418, 249);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(146, 53);
+            this.btnRead.TabIndex = 12;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = false;
+            this.btnRead.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(22, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Cinsiyet";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtCinsiyet
+            // 
+            this.txtCinsiyet.Location = new System.Drawing.Point(205, 270);
+            this.txtCinsiyet.Multiline = true;
+            this.txtCinsiyet.Name = "txtCinsiyet";
+            this.txtCinsiyet.Size = new System.Drawing.Size(144, 25);
+            this.txtCinsiyet.TabIndex = 15;
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(205, 26);
+            this.txtNo.Multiline = true;
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(144, 25);
+            this.txtNo.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(22, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 25);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Personel No";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtNameFol
+            // 
+            this.txtNameFol.Location = new System.Drawing.Point(814, 26);
+            this.txtNameFol.Multiline = true;
+            this.txtNameFol.Name = "txtNameFol";
+            this.txtNameFol.Size = new System.Drawing.Size(125, 26);
+            this.txtNameFol.TabIndex = 19;
+            this.txtNameFol.TextChanged += new System.EventHandler(this.txtNameFol_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(648, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 25);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Ad Filtrele";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(814, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 27);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Yaş Sırala";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.Location = new System.Drawing.Point(814, 140);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 29);
+            this.radioButton1.TabIndex = 29;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Bay";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton2.Location = new System.Drawing.Point(928, 140);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(91, 29);
+            this.radioButton2.TabIndex = 30;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Bayan";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(648, 140);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(101, 25);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "Cinsiyet :";
+            this.label8.Click += new System.EventHandler(this.label8_Click_1);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(648, 84);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(111, 25);
+            this.label9.TabIndex = 32;
+            this.label9.Text = "Yaş Sırala";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1127, 673);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtNameFol);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.txtCinsiyet);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateDogumT);
+            this.Controls.Add(this.txtMaas);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtMaas;
+        private System.Windows.Forms.DateTimePicker dateDogumT;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCinsiyet;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNameFol;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter da;
+        DataTable tablo=new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void PersonelGetir()
+        {
+            baglanti=new SqlConnection("server=(localdb)\\MSSQLLocalDB;Initial Catalog=sirket;Integrated Security=SSPI");
+            baglanti.Open();
+            da = new SqlDataAdapter("SELECT * FROM personel", baglanti);
+            DataTable tablo=new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PersonelGetir();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string sorgu = "SELECT Max(@maas) FROM personel";
+            komut = new SqlCommand(sorgu, baglanti);
+            //komut.Parameters.AddWithValue("@personel_no", txtNo.Text);
+            //komut.Parameters.AddWithValue("@ad", txtAd.Text);
+            //komut.Parameters.AddWithValue("@soyad", txtSoyad.Text);
+            //komut.Parameters.AddWithValue("@dogum_tarihi", dateDogumT.Value);
+            komut.Parameters.AddWithValue("@maas", txtMaas.Text);
+            //komut.Parameters.AddWithValue("@cinsiyet", txtCinsiyet.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            PersonelGetir();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            txtNo.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtAd.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtSoyad.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            dateDogumT.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            txtMaas.Text = dataGridView1.CurrentRow.Cells[8].Value.ToString();
+            txtCinsiyet.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string sorgu = "INSERT INTO personel(ad,soyad,dogum_tarihi,maas,cinsiyet) VALUES(@ad,@soyad,@dogum_tarihi,@maas,@cinsiyet)";
+            komut = new SqlCommand(sorgu,baglanti);
+            //komut.Parameters.AddWithValue("@personel_no", txtNo.Text);
+            komut.Parameters.AddWithValue("@ad", txtAd.Text);
+            komut.Parameters.AddWithValue("@soyad", txtSoyad.Text);
+            komut.Parameters.AddWithValue("@dogum_tarihi", dateDogumT.Value);
+            komut.Parameters.AddWithValue("@maas", txtMaas.Text);
+            komut.Parameters.AddWithValue("@cinsiyet", txtCinsiyet.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            PersonelGetir();
+
+        }
+
+        private void dateDogumT_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            string sorgu = "DELETE FROM personel where personel_no=@personel_no";
+            komut=new SqlCommand(sorgu,baglanti);
+            komut.Parameters.AddWithValue("@personel_no", Convert.ToInt32(txtNo.Text));
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            PersonelGetir();
+        
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            string sorgu = "UPDATE personel set ad=@ad,soyad=@soyad,dogum_tarihi=@dogum_tarihi,maas=@maas,cinsiyet=@cinsiyet where personel_no=@personel_no";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@personel_no", Convert.ToInt32(txtNo.Text));
+            komut.Parameters.AddWithValue("@ad",txtAd.Text);
+            komut.Parameters.AddWithValue("@soyad", txtSoyad.Text);
+            komut.Parameters.AddWithValue("@dogum_tarihi", dateDogumT.Value);
+            komut.Parameters.AddWithValue("@maas", txtMaas.Text);
+            komut.Parameters.AddWithValue("@cinsiyet", txtCinsiyet.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            PersonelGetir();
+        }
+
+        private void txtNameFol_TextChanged(object sender, EventArgs e)
+        {
+            tablo.Clear();
+            komut = new SqlCommand("AdFiltrele", baglanti);
+            komut.CommandType = CommandType.StoredProcedure;
+            komut.Parameters.AddWithValue("Ad",txtNameFol.Text);    
+            da = new SqlDataAdapter(komut);
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+        }
+
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tablo.Clear();
+            komut = new SqlCommand("den44", baglanti);
+            komut.CommandType = CommandType.StoredProcedure;
+            da = new SqlDataAdapter(komut);
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            tablo.Clear();
+            komut = new SqlCommand("den45", baglanti);
+            komut.CommandType = CommandType.StoredProcedure;
+            da = new SqlDataAdapter(komut);
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            tablo.Clear();
+            komut = new SqlCommand("den46", baglanti);
+            komut.CommandType = CommandType.StoredProcedure;
+            da = new SqlDataAdapter(komut);
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSoyad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
